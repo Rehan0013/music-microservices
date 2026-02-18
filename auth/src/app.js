079f4f4599d2532d4eee342a003ada97c9c8119b
Rehan-013 <153925483+Rehan-013@users.noreply.github.com>
2026-02-18 13:40:03 +0530

    feat: Configure CORS for auth and music services with specific origin and credentials, and remove unused email utility import from notification service.

--- auth/src/app.js
+++ auth/src/app.js
@@ -19,7 +19,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(morgan("dev"));
-app.use(cors());
+app.use(cors(
+    {
+        origin: "http://localhost:5173",
+        credentials: true
+    }
+));
 
 app.use(passport.initialize());
 // Configure Passport to use Google OAuth 2.0 strategy
--- notification/src/app.js
+++ notification/src/app.js
@@ -1,7 +1,5 @@
 import express from "express";
 
-import sendEmail from "./utils/email.js";
-
 const app = express();
 
 
